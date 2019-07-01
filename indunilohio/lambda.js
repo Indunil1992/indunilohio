946c44da9b739af26a2b70066cb1a6aa9f76cfdf
Indunil1992 <indunil@adroitlogic.com>
2019-07-01 16:03:15 +0530

    l

--- indunilohio/lambda.js
+++ indunilohio/lambda.js
@@ -1,6 +1,25 @@
-exports.handler = function(event, context, callback) {
+let AWS = require('aws-sdk');
+const cognito_idp = new AWS.CognitoIdentityServiceProvider();
+
+exports.handler = function (event, context, callback) {
+
+    cognito_idp.adminCreateUser({
+        UserPoolId: process.env.UserPoolId_cognitoresource,
+        Username: "indunil",
+        DesiredDeliveryMediums: ["EMAIL", "SMS"],
+        ForceAliasCreation: false,
+        TemporaryPassword: "Sachithra@142",
+        UserAttributes: [],
+        ValidationData: [],
+        MessageAction: "RESEND"
+    }, function (error, data) {
+        if (error) {
+            // implement error handling logic here
+            throw error;
+        }
+        // your logic goes within this block
+    });
 
 
-    
-    callback(null, {"message": "Successfully *** executed"});
+    callback(null, { "message": "Successfully *** executed" });
 }
